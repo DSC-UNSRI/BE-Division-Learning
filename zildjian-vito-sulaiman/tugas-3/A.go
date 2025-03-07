794c2bb71807a42df5b36fb54407584e1d5bd4d5
zildjianvitoo <zildjianvito07@gmail.com>
2025-03-07 23:10:10 +0700

    feat: use go mod for consistency and solve reverse number problem

--- zildjian-vito-sulaiman/tugas-3/A.go
+++ zildjian-vito-sulaiman/tugas-3/A.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-func main() {
+func MainBiggestModus() {
 	var n int
 	fmt.Scan(&n)
 
 	counts := make(map[int]int)
-	maxCount, modusTerbesar := 0, 0
+	maxCount, biggestModus := 0, 0
 
 	for i := 0; i < n; i++ {
 		var num int
@@ -16,11 +16,11 @@ func main() {
 
 		switch {
 		case counts[num] > maxCount:
-			maxCount, modusTerbesar = counts[num], num
-		case counts[num] == maxCount && num > modusTerbesar:
-			modusTerbesar = num
+			maxCount, biggestModus = counts[num], num
+		case counts[num] == maxCount && num > biggestModus:
+			biggestModus = num
 		}
 	}
 
-	fmt.Println(modusTerbesar)
+	fmt.Println(biggestModus)
 }
