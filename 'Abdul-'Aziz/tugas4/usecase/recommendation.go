11b9f60b457fd1c90032dd7dc9eeaaac4bec1ffa
Abdul Aziz <bintangkapala@gmail.com>
2025-03-14 16:58:20 +0700

    feat: add recommendation  usecase

--- 'Abdul-'Aziz/tugas4/usecase/recommendation.go
+++ 'Abdul-'Aziz/tugas4/usecase/recommendation.go
@@ -1,1 +1,46 @@
 package usecase
+
+import (
+	"errors"
+
+	"tugas/tugas4/models"
+	"tugas/tugas4/repository"
+)
+
+type RecommendationUseCase struct {
+    repo repository.UserRepository
+}
+
+func NewRecommendationUseCase(repo repository.UserRepository) *RecommendationUseCase {
+    return &RecommendationUseCase{repo: repo}
+}
+
+func (uc *RecommendationUseCase) AddRecommendation(category, content string) error {
+    user, err := uc.repo.GetUser()
+    if err != nil {
+        return err
+    }
+    
+    user.Recommendations = append(user.Recommendations, models.Recommendation{
+        Category: category,
+        Content:  content,
+    })
+    return uc.repo.SaveUser(user)
+}
+
+func (uc *RecommendationUseCase) UpdateRecommendation(index int, category, content string) error {
+    user, err := uc.repo.GetUser()
+    if err != nil {
+        return err
+    }
+    
+    if index < 0 || index >= len(user.Recommendations) {
+        return errors.New("index rekomendasi tidak valid")
+    }
+    
+    user.Recommendations[index] = models.Recommendation{
+        Category: category,
+        Content:  content,
+    }
+    return uc.repo.SaveUser(user)
+}
