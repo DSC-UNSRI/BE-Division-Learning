7b1743196ce471972d34beef2b2ad67df6c96aae
Abdul Aziz <bintangkapala@gmail.com>
2025-03-14 16:58:57 +0700

    feat: add friend usecase

--- 'Abdul-'Aziz/tugas4/usecase/friend.go
+++ 'Abdul-'Aziz/tugas4/usecase/friend.go
@@ -1,1 +1,46 @@
 package usecase
+
+import (
+	"errors"
+
+	"tugas/tugas4/models"
+	"tugas/tugas4/repository"
+)
+
+type FriendUseCase struct {
+    repo repository.UserRepository
+}
+
+func NewFriendUseCase(repo repository.UserRepository) *FriendUseCase {
+    return &FriendUseCase{repo: repo}
+}
+
+func (uc *FriendUseCase) AddFriend(name, division string) error {
+    user, err := uc.repo.GetUser()
+    if err != nil {
+        return err
+    }
+    
+    user.Friends = append(user.Friends, models.Friend{
+        Name:     name,
+        Division: division,
+    })
+    return uc.repo.SaveUser(user)
+}
+
+func (uc *FriendUseCase) UpdateFriend(index int, name, division string) error {
+    user, err := uc.repo.GetUser()
+    if err != nil {
+        return err
+    }
+    
+    if index < 0 || index >= len(user.Friends) {
+        return errors.New("index teman tidak valid")
+    }
+    
+    user.Friends[index] = models.Friend{
+        Name:     name,
+        Division: division,
+    }
+    return uc.repo.SaveUser(user)
+}
