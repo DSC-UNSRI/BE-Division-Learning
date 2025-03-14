d7e2d1dc1a59d79a2f82d81f9e2c1665944e53eb
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-03-14 17:06:16 +0700

    feat: menambahkan entry point aplikasi

---
+++ ahmad-fadhil-rizqi/backend-iftar-gdgoc/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"backend-iftar-gdgoc/config"
+	"backend-iftar-gdgoc/controllers"
+	"fmt"
+	"log"
+)
+
+func main() {
+	err := config.LoadEnv()
+	if err != nil {
+		log.Fatal("Gagal memuat konfigurasi")
+	}
+
+	if !controllers.CekAutentikasi() {
+		fmt.Println("Autentikasi gagal. Cek kembali email dan password.")
+		return
+	}
+
+	controllers.Dashboard()
+}
