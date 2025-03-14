1df249f803c0ca5800f0a1c920a3e072966014e8
RenaiRS <dianramadhan71005@gmail.com>
2025-03-14 17:02:53 +0700

    feat: file auth.go

---
+++ controllers/auth.go
@@ -0,0 +1,38 @@
+package controllers
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"Task_4/models"
+)
+
+func LoadEnv() models.User {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return models.User{
+		Name:     os.Getenv("NAMA"),
+		Email:    os.Getenv("EMAIL"),
+		Password: os.Getenv("PASSWORD"),
+	}
+}
+func Autentikasi(user models.User) bool {
+	var email, password string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Masukkan Email: ")
+	scanner.Scan()
+	email = scanner.Text()
+
+	fmt.Print("Masukkan Password: ")
+	scanner.Scan()
+	password = scanner.Text()
+
+	return email == user.Email && password == user.Password
+}
