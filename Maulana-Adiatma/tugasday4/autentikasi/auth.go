37888ae43f2be7eebd9923c76f6ed7684f92cda0
mauinGit <maulanaadiatma3133@gmail.com>
2025-03-17 22:59:33 +0700

    feat: menambahkan folder autentikasi
    gunanya untuk bisa mengakses dashboard misal ketiga indikator itu terpnuhi

---
+++ Maulana-Adiatma/tugasday4/autentikasi/auth.go
@@ -0,0 +1,31 @@
+package autentikasi
+
+import (
+	"fmt"
+	"os"
+	"github.com/joho/godotenv"
+)
+
+func Login() bool {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return false
+	}
+
+	var userName, email, password string
+	fmt.Print("Masukkan username: ")
+	fmt.Scan(&userName)
+	fmt.Print("Masukkan email: ")
+	fmt.Scan(&email)
+	fmt.Print("Masukkan password: ")
+	fmt.Scan(&password)
+
+	if email == os.Getenv("EMAIL") && password == os.Getenv("PASSWORD") {
+		fmt.Println("Login berhasil!")
+		return true
+	} else {
+		fmt.Println("Email atau password salah")
+		return false
+	}
+}
