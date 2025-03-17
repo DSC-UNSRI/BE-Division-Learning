03a8632b8f885e7054107ca7195f76fb53dc865c
mauinGit <maulanaadiatma3133@gmail.com>
2025-03-18 01:11:13 +0700

    refactor: output rekomendasi

--- Maulana-Adiatma/tugasday4/dashboard/sistem_utama.go
+++ Maulana-Adiatma/tugasday4/dashboard/sistem_utama.go
@@ -10,7 +10,7 @@ func MenuDashboard() {
 		fmt.Println("=== Dashboard ===")
 		fmt.Println("1. Pilih Kendaraan")
 		fmt.Println("2. Tambah Barang Bawaan")
-		fmt.Println("3. Tambah Rekomendasi Kegiatan")
+		fmt.Println("3. Tambah Rekomendasi")
 		fmt.Println("4. Ajak Teman")
 		fmt.Println("5. Lihat Semua Data")
 		fmt.Println("6. Keluar")
