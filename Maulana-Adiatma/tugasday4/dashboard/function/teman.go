45af309317ef59bb20233179cf5724890df6da6d
mauinGit <maulanaadiatma3133@gmail.com>
2025-03-18 01:24:17 +0700

    fix: memasukkan program

--- Maulana-Adiatma/tugasday4/dashboard/function/teman.go
+++ Maulana-Adiatma/tugasday4/dashboard/function/teman.go
@@ -1,1 +1,24 @@
+package dashboard
 
+import "fmt"
+
+type Teman struct {
+	Nama   string
+	Divisi string
+}
+
+var daftarTeman []Teman
+
+func Menambahteman() {
+	var nama, divisi string
+
+	fmt.Println("=== Tambah Teman yang Ikut Iftar ===")
+	fmt.Print("Masukkan nama teman: ")
+	fmt.Scan(&nama)
+	fmt.Print("Masukkan divisi teman: ")
+	fmt.Scan(&divisi)
+
+	temanBaru := Teman{Nama: nama, Divisi: divisi}
+	daftarTeman = append(daftarTeman, temanBaru)
+	fmt.Println("Teman berhasil ditambahkan!")
+}
