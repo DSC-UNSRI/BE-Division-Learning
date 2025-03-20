245716462a67bf54fb4dbdecf851ec8b587449fc
Steven <stevenbae9@gmail.com>
2025-03-20 13:06:54 +0700

    fix: update struct data type for temanController and rekomendasiController error

--- steven/tugas4/models/Dashboard.go
+++ steven/tugas4/models/Dashboard.go
@@ -3,6 +3,6 @@ package models
 type Dashboard struct {
 	Kendaraan        string
 	Barang           []string
-	Rekomendasi      map[string]string
-	Teman            map[string]string
+	Rekomendasi      map[string][]string
+	Teman            map[string][]string
 }
