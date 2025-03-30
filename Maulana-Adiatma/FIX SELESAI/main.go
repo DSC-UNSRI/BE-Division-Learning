ae9c20c31d558c19215eed1aad09a80437c696d6
mauinGit <maulanaadiatma3133@gmail.com>
2025-03-30 13:57:33 +0700

    feat: main.go
    menghubungkan main.go dengan auth.go

--- Maulana-Adiatma/FIX SELESAI/main.go
+++ Maulana-Adiatma/FIX SELESAI/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	"cobafix.go/autentikasi"
 )
 
 func main() {
 	fmt.Println("Silakan login terlebih dahulu")
+	if autentikasi.Login() {
+		fmt.Println("Akses diberikan")
+	} else {
+		fmt.Println("Akses ditolak")
+	}
 }
