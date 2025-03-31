c3e64e5a887fdee07a1b14679a5d218d533d8f82
mauinGit <maulanaadiatma3133@gmail.com>
2025-03-31 21:12:59 +0700

    fix: main.go
    mengarahkan user ke dashboard.go (tampilan utama supaya user bisa menggunakan nya, tadi tu cuman login doang KWKWKWKWK)

--- Maulana-Adiatma/FIX SELESAI/main.go
+++ Maulana-Adiatma/FIX SELESAI/main.go
@@ -10,6 +10,7 @@ func main() {
 	fmt.Println("Silakan login terlebih dahulu")
 	if autentikasi.Login() {
 		fmt.Println("Akses diberikan")
+		dashboard.Dashboard()
 	} else {
 		fmt.Println("Akses ditolak")
 	}
