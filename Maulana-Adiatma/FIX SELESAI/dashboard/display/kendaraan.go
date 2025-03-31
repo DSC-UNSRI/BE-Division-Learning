16734a7fc6eacef352f029db63fbb9e9a5c98878
mauinGit <maulanaadiatma3133@gmail.com>
2025-03-31 21:32:45 +0700

    refactor: kendaraan.go
    cuman nambah \n dibeberapa line

--- Maulana-Adiatma/FIX SELESAI/dashboard/display/kendaraan.go
+++ Maulana-Adiatma/FIX SELESAI/dashboard/display/kendaraan.go
@@ -11,9 +11,9 @@ func TampilkanKendaraan() {
 
 		
 		if selected == "" {
-			fmt.Println("kamu belum memilih kendaraan! Silakan pilih kendaraan:")
+			fmt.Println("\nkamu belum memilih kendaraan! Silakan pilih kendaraan:")
 		} else {
-			fmt.Println("kamu sudah memilih:", selected)
+			fmt.Println("\nkamu sudah memilih:", selected)
 			fmt.Print("kamu ingin memperbarui kendaraan? (y/n): ")
 
 			var updateChoice string
@@ -25,7 +25,7 @@ func TampilkanKendaraan() {
 			}
 		}
 
-		fmt.Println("Pilih kendaraan untuk pergi ke iftar:")
+		fmt.Println("\nPilih kendaraan untuk pergi ke iftar:")
 		fmt.Println("1. Kendaraan Pribadi")
 		fmt.Println("2. Bus Kaleng")
 		fmt.Println("3. Nebeng")
