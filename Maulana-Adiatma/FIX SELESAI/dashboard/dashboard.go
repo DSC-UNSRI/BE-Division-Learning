b059b2c70a18f13621f387286e662dfbf01aac0a
mauinGit <maulanaadiatma3133@gmail.com>
2025-04-01 09:11:37 +0700

    feat: dashboard.go
    menambahkan opsi untuk user memilih pilihan teman

--- Maulana-Adiatma/FIX SELESAI/dashboard/dashboard.go
+++ Maulana-Adiatma/FIX SELESAI/dashboard/dashboard.go
@@ -12,9 +12,10 @@ func Dashboard() {
 		fmt.Println("1. Pilih kendaraan")
 		fmt.Println("2. Pilih Barang")
 		fmt.Println("3. Pilih Rekomendasi")
-		fmt.Println("4. Lihat semua data")
-		fmt.Println("5. Keluar")
-		fmt.Print("Masukkan pilihan (1-5): ")
+		fmt.Println("4. Pilih Teman")
+		fmt.Println("5. Lihat semua data")
+		fmt.Println("6. Keluar")
+		fmt.Print("Masukkan pilihan (1-6): ")
 
 		var menu int
 		fmt.Scan(&menu)
@@ -28,12 +29,14 @@ func Dashboard() {
 		case 3:
 			display.TampilkanRekomendasi()
 		case 4:
-			display.LihatSemuaData()
+			display.TampilkanTeman()
 		case 5:
+			display.LihatSemuaData()
+		case 6:
 			fmt.Println("Terima kasih! Keluar dari program.")
 			return
 		default:
-			fmt.Println("❌ Pilihan tidak valid, coba lagi!")
+			fmt.Println("Pilihan tidak valid, coba lagi")
 		}
 	}
 }
