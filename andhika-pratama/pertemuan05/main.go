aa6ab103807fc732007884037333ce4613cce9ee
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-04-12 21:28:38 +0700

    feat: add database connection

--- andhika-pratama/pertemuan05/main.go
+++ andhika-pratama/pertemuan05/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"pertemuan05/config"
+	"pertemuan05/database"
+
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +11,8 @@ import (
 
 func main() {
 	config.ENVLoad()
+	database.InitDB()
+	defer database.DB.Close()
 
 	fmt.Println("Server running at http://localhost:8080/")
 	log.Fatal(http.ListenAndServe(":8080", nil))
