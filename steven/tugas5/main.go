3e74776c6ad4ad808bb53bc61c8ccbd484687ce8
Steven <stevenbae9@gmail.com>
2025-04-12 20:59:03 +0700

    init: make architecture

---
+++ steven/tugas5/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"tugas5/config"
+	"tugas5/database"
+	"tugas5/routes"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config.ENVLoad()
+	database.InitDB()
+	defer database.DB.Close()
+	database.Migrate()
+	routes.BooksRoutes()
+
+	fmt.Println("Server running at http://localhost:8080/")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
