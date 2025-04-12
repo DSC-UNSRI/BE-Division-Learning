21a4701196f0199dd68b132577a2b84e49215d84
RenaiRS <dianramadhan71005@gmail.com>
2025-04-12 16:49:23 +0700

    Feat: Dashboard

---
+++ Dian-Kharisma-Ramadhan/Task_4/README.md
@@ -0,0 +1,7 @@
+SISTEM DASHBOARD IFTAR GDGoC
+CARA MENJALANKAN:
+   - Jalankan `go mod tidy` untuk mengunduh dependensi.
+   - Jalankan dengan perintah:
+     go run main.go
+   - terdapat 5 opsi di dashboar
+   - bisa dipilih untuk dijawab oleh user
---
+++ Dian-Kharisma-Ramadhan/Task_4/controllers/menu.go
@@ -0,0 +1,14 @@
+package controllers
+
+import "fmt"
+
+func ShowMenu() {
+	fmt.Println("\nDASHBOARD IFTAR GDGoC")
+	fmt.Println("1. Pilih Kendaraan")
+	fmt.Println("2. Tambah Barang")
+	fmt.Println("3. Tambah Rekomendasi Iftar")
+	fmt.Println("4. Tambah Teman yang Ikut")
+	fmt.Println("5. Lihat Semua Data")
+	fmt.Println("6. Exit")
+	fmt.Print("Pilih menu: ")
+}
