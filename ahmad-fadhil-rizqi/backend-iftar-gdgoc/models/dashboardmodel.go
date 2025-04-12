df2472eced0430d2347eed4f8b6a009ac8caf150
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-04-12 15:38:44 +0700

    feat: add model dashboardmodel.go

---
+++ ahmad-fadhil-rizqi/backend-iftar-gdgoc/models/dashboardmodel.go
@@ -0,0 +1,19 @@
+package models
+
+type Barang struct {
+	Nama string
+}
+
+type Vehicle struct {
+	Name string
+}
+
+type Rekomendasi struct {
+	Kategori string
+	Isi      string
+}
+
+type Teman struct {
+	Nama   string
+	Divisi string
+}
