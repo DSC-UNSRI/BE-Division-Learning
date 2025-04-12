da5c592cae9dd0d0344b8e01b62711f3ee25ce90
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-04-12 21:22:54 +0700

    feat: add main.go

--- andhika-pratama/pertemuan05/config/env.go
+++ andhika-pratama/pertemuan05/config/env.go
@@ -8,7 +8,7 @@ import (
 
 func ENVLoad() {
 	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
 }
