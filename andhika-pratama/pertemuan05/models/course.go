4fc600799770a650be1898e5b3a8c1635b8c5810
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-04-13 14:49:53 +0700

    refactor(database): change the wrongly-assigned types

--- andhika-pratama/pertemuan05/models/course.go
+++ andhika-pratama/pertemuan05/models/course.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type Course struct {
-	CourseID   int        `json:"course_id"`
-	CourseName       string     `json:"course_name"`
+	CourseID   string     `json:"course_id"`
+	CourseName string     `json:"course_name"`
 	LecturerID int        `json:"lecturer_id"`
 	Semester   int        `json:"semester"`
 	Credit     int        `json:"credit"`
@@ -15,7 +15,7 @@ var CourseQuery = `
 	CREATE TABLE IF NOT EXISTS courses (
 		course_id VARCHAR(6) PRIMARY KEY,
 		course_name VARCHAR(40) NOT NULL,
-		lecturer_id INT NOT NULL,
+		lecturer_id VARCHAR(6) NOT NULL,
 		semester INT NOT NULL,
 		credit INT NOT NULL,
 		deleted_at TIMESTAMP NULL DEFAULT NULL,
