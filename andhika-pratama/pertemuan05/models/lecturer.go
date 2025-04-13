14bd46b8e9f336e0730477cb113c55c7a9f21ed9
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-04-13 14:45:33 +0700

    refactor(database): change lecturer_id and course_id type from int to string

--- andhika-pratama/pertemuan05/models/lecturer.go
+++ andhika-pratama/pertemuan05/models/lecturer.go
@@ -3,16 +3,16 @@ package models
 import "time"
 
 type Lecturer struct {
-	ID        int        `json:"lecturer_id"`
-	Name      string     `json:"name"`
+	LecturerID        int        `json:"lecturer_id"`
+	LecturerName      string     `json:"lecturer_name"`
 	Password  string     `json:"password"`
 	DeletedAt *time.Time `json:"deleted_at"`
 }
 
 var LecturerQuery = `
 	CREATE TABLE IF NOT EXISTS lecturers (
-		lecturer_id INT AUTO_INCREMENT PRIMARY KEY,
-		name VARCHAR(40) NOT NULL,
+		lecturer_id VARCHAR(3) PRIMARY KEY,
+		lecturer_name VARCHAR(40) NOT NULL,
 		password VARCHAR(30) NOT NULL,
 		deleted_at TIMESTAMP NULL DEFAULT NULL
 	);`
