82414f597bfc1811b4c6344edf7c28b27d2f4edc
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-04-13 15:44:43 +0700

    feat: add login authentication

--- andhika-pratama/pertemuan05/main.go
+++ andhika-pratama/pertemuan05/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"pertemuan05/config"
 	"pertemuan05/database"
+	"pertemuan05/routes"
 
 	"fmt"
 	"log"
@@ -14,6 +15,7 @@ func main() {
 	database.InitDB()
 	defer database.DB.Close()
 	database.Migrate()
+	routes.RoutesHandlers()
 
 	fmt.Println("Server running at http://localhost:8080/")
 	log.Fatal(http.ListenAndServe(":8080", nil))
