44ed56ca4ec9d9030a59a7c9de207d5ca80839b9
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-04-13 18:18:48 +0700

    feat: add model address

---
+++ andhika-pratama/pertemuan05/models/address.go
@@ -0,0 +1,21 @@
+package models
+
+import "time"
+
+type Address struct {
+	AddressID  int    `json:"address_id"`
+	LecturerID string `json:"lecturer_id"`
+	Street     string `json:"street"`
+	City       string `json:"city"`
+	DeletedAt  *time.Time `json:"deleted_at"`
+}
+
+var AddressQuery = `
+	CREATE TABLE addresses (
+		address_id INT AUTO_INCREMENT PRIMARY KEY,
+		lecturer_id VARCHAR(3) NOT NULL,
+		street VARCHAR(60),
+		city VARCHAR(30),
+		deleted_at TIMESTAMP NULL DEFAULT NULL,
+		FOREIGN KEY (lecturer_id) REFERENCES lecturers(lecturer_id)
+	);`
