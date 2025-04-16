5e8d83789656a1d3dca396d3fb5dbe0fdf8512b9
Pandawa13 <142719404+Pandawa13@users.noreply.github.com>
2025-04-16 22:01:59 +0700

    chore(database): add mysql db connection using .env

---
+++ Ajun-Elvas/playlist-app/database/connection.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASS") + "@tcp(" + os.Getenv("DB_HOST") + ")/" + os.Getenv("DB_NAME")
+	DB, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = DB.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Database connection established")
+}
