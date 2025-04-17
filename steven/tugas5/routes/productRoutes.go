27d5b56acab08d96c1236d90ddb88c965d289740
Steven <stevenbae9@gmail.com>
2025-04-18 02:24:29 +0700

    refactror: separate route files for every endpoint model

--- steven/tugas5/routes/productRoutes.go
+++ steven/tugas5/routes/productRoutes.go
@@ -11,12 +11,6 @@ func ProductsRoutes(){
 	http.HandleFunc("/products/", productsHandlerWithID)
 }
 
-func StoreRoutes(){
-	http.HandleFunc("/store", storeHandler)
-	http.HandleFunc("/store/", storeHandlerWithID)
-	http.HandleFunc("/store/auth", storeAuthHandler)
-}
-
 func productsHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
@@ -52,33 +46,6 @@ func storeHandler(w http.ResponseWriter, r *http.Request) {
 		controllers.GetStores(w, r)
 	case http.MethodPost:
 		controllers.CreateStore(w, r)
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
-}
-
-func storeHandlerWithID(w http.ResponseWriter, r *http.Request) {
-	parts, err := utils.SplitPath(r.URL.Path)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-	}
-	id := parts[2]
-	switch r.Method {
-	case http.MethodGet:
-		controllers.GetStore(w, r, id)
-	case http.MethodPatch:
-		controllers.UpdateStore(w, r, id)
-	case http.MethodDelete:
-		controllers.DeleteStore(w, r, id)
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
-}
-
-func storeAuthHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodPost:
-		controllers.AuthStore(w, r)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
