33443160caa7f7487606014dcba7b45a5b2f8bbf
mauinGit <maulanaadiatma3133@gmail.com>
2025-04-17 21:48:27 +0700

    feat: add resep_control.go

---
+++ Maulana-Adiatma/Resep_Makanan/controllers/resep_control.go
@@ -0,0 +1,106 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"percobaan3/database"
+	"percobaan3/models"
+
+	"github.com/gorilla/mux"
+)
+
+func GetAllResep(w http.ResponseWriter, r *http.Request) {
+	rows, err := database.DB.Query("SELECT * FROM data_resep")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var reseps []models.Resep
+	for rows.Next() {
+		var r models.Resep
+		if err := rows.Scan(&r.ID, &r.NamaResep, &r.DeskripsiResep); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		reseps = append(reseps, r)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(reseps)
+}
+
+func GetResepByID(w http.ResponseWriter, r *http.Request) {
+	idParam := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(idParam)
+
+	var rcp models.Resep
+	err := database.DB.QueryRow("SELECT * WHERE id = ?", id).
+		Scan(&rcp.ID, &rcp.NamaResep, &rcp.DeskripsiResep)
+
+	if err != nil {
+		http.Error(w, "Resep not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(rcp)
+}
+
+func CreateResep(w http.ResponseWriter, r *http.Request) {
+	var rcp models.Resep
+	err := json.NewDecoder(r.Body).Decode(&rcp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	_, err = database.DB.Exec("INSERT INTO data_resep(nama_resep, description) VALUES(?, ?)",
+		rcp.NamaResep, rcp.DeskripsiResep)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Resep created"))
+}
+
+func UpdateResep(w http.ResponseWriter, r *http.Request) {
+	idParam := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(idParam)
+
+	var rcp models.Resep
+	err := json.NewDecoder(r.Body).Decode(&rcp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	_, err = database.DB.Exec("UPDATE data_resep SET nama_resep=?, description=? WHERE id=?",
+		rcp.NamaResep, rcp.DeskripsiResep, id)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("Resep updated"))
+}
+
+func DeleteResep(w http.ResponseWriter, r *http.Request) {
+	idParam := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(idParam)
+
+	_, err := database.DB.Exec("DELETE FROM data_resep WHERE id=?", id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("Resep deleted"))
+}
