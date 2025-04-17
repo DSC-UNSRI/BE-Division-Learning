085056b68b4bf6d5b39a8a579922027337be161d
barbarIQ <rihalsulaiman@gmail.com>
2025-04-17 22:30:46 +0700

    feat: add new file error.go

---
+++ Adhia-Rihal-Sulaiman/tugas05/utils/error.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// HandleError - Menangani error umum dan mengirim response standar
+func HandleError(ctx *gin.Context, statusCode int, errMessage string) {
+	log.Println("Error:", errMessage)
+	ctx.JSON(statusCode, gin.H{
+		"error": errMessage,
+	})
+}
