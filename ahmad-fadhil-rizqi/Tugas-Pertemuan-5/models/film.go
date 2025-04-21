6c979f38d8c99c0c3d41d7b82bef38befcec4712
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-04-21 12:00:07 +0700

    fix: Update Film model to use DirectorID

--- ahmad-fadhil-rizqi/Tugas-Pertemuan-5/models/film.go
+++ ahmad-fadhil-rizqi/Tugas-Pertemuan-5/models/film.go
@@ -3,17 +3,18 @@ package models
 import "time"
 
 type Film struct {
-	ID        int        `json:"id"`
-	Title     string     `json:"title"`
-	Director  string     `json:"director"`
-	DeletedAt *time.Time `json:"-"`
+	ID         int        `json:"id"`
+	Title      string     `json:"title"`
+	DirectorID int        `json:"director_id"`
+	DeletedAt  *time.Time `json:"-"`
 }
 
 var FilmsQuery = `
 	CREATE TABLE IF NOT EXISTS films (
 		id INT AUTO_INCREMENT PRIMARY KEY,
 		title VARCHAR(100) NOT NULL,
-		director VARCHAR(100) NOT NULL,
-		deleted_at TIMESTAMP NULL DEFAULT NULL
+		director_id INT NOT NULL,
+		deleted_at TIMESTAMP NULL DEFAULT NULL,
+		FOREIGN KEY (director_id) REFERENCES directors(id) ON DELETE RESTRICT ON UPDATE CASCADE
 	);
 `
