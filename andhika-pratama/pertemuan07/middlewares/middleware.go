746bd2b0e9a5001ce2c5dd995f81c0bccb2cad6b
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-05-29 01:35:49 +0700

    feat(middleware): add a middleware to check the ownership of a course

--- andhika-pratama/pertemuan07/middlewares/middleware.go
+++ andhika-pratama/pertemuan07/middlewares/middleware.go
@@ -45,11 +45,41 @@ func AuthMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-func AdminMiddleware(next http.Handler) http.Handler {
+func OldMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		role := r.Context().Value(utils.RoleKey)
 		if role == nil || role.(string) != "old" {
 			http.Error(w, "Forbidden - old lecturers access only", http.StatusForbidden)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func CourseOwnershipMiddleware(next http.Handler, courseID string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctxLecturerID := r.Context().Value(utils.LecturerIDKey)
+		lecturerID, ok := ctxLecturerID.(string)
+		if !ok || lecturerID == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		if courseID == "" {
+			http.Error(w, "course_id is null", http.StatusBadRequest)
+			return
+		}
+
+		var courseOwnerID string
+		err := database.DB.QueryRow("SELECT lecturer_id FROM courses WHERE course_id = ? AND deleted_at IS NULL", courseID).Scan(&courseOwnerID)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		if courseOwnerID != lecturerID {
+			http.Error(w, "Forbidden - the course isn't yours", http.StatusForbidden)
 			return
 		}
 
