d4b0e9593de5461f2745b9ffe2fd809b9361beba
barbarIQ <rihalsulaiman@gmail.com>
2025-05-31 14:27:04 +0700

    feat: add context utility for ChefID and role keys

--- Adhia-Rihal-Sulaiman/tugas07/models/menu.go
+++ Adhia-Rihal-Sulaiman/tugas07/models/menu.go
@@ -1,14 +1,14 @@
-package models  
+package models
 
 import "time"
 
-type Menu struct {  
-	ID          int     `json:"id"`  
-	Name        string  `json:"name"`  
-	Description string  `json:"description"`  
-	Price       int `json:"price"`  
-	ChefID      int     `json:"chef_id"`  
-	Category    string  `json:"category"`  
+type Menu struct {
+	ID          int        `json:"id"`
+	Name        string     `json:"name"`
+	Description string     `json:"description"`
+	Price       int        `json:"price"`
+	ChefID      int        `json:"chef_id"`
+	Category    string     `json:"category"`
 	DeletedAt   *time.Time `json:"deleted_at"`
 }
 
@@ -21,5 +21,5 @@ var MenuQuery = `
 		chef_id INT,
 		category VARCHAR(30),
 		deleted_at TIMESTAMP NULL DEFAULT NULL,
-		FOREIGN KEY (chef_id) REFERENCES chefs(id)
+		FOREIGN KEY (chef_id) REFERENCES chefs(chef_id)
 	);`
