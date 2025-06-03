00aa8d3466bd3b65049186a14b7737dc8972696d
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-06-03 18:28:47 +0700

    refactor(routes): split routes into different files

--- andhika-pratama/uts-andhika-pratama/routes/route.go
+++ andhika-pratama/uts-andhika-pratama/routes/route.go
@@ -1,13 +1,5 @@
 package routes
 
-import (
-	"uts/controllers"
-
-	"net/http"
-)
-
 func SetupRoutes() {
-	http.HandleFunc("/register", controllers.Register)
-	http.HandleFunc("/login", controllers.Login)
-	// http.HandleFunc("/forget", controllers.ForgetPassword)
+	AuthRoutes()
 }
