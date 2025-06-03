e737e5a3011c77779475425117007599e783c253
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-06-03 19:07:14 +0700

    feat(models): enhance Director struct with PasswordHash, Token, and Role fields

--- ahmad-fadhil-rizqi/Tugas-Pertemuan-7/models/director.go
+++ ahmad-fadhil-rizqi/Tugas-Pertemuan-7/models/director.go
@@ -3,15 +3,21 @@ package models
 import "time"
 
 type Director struct {
-	ID        int        `json:"id"`
-	Name      string     `json:"name"`
-	DeletedAt *time.Time `json:"-"`
+	ID           int        `json:"id"`
+	Name         string     `json:"name"`
+	PasswordHash string     `json:"-"`
+	Token        string     `json:"token"`
+	Role         string     `json:"role"`
+	DeletedAt    *time.Time `json:"-"`
 }
 
 var DirectorsQuery = `
 	CREATE TABLE IF NOT EXISTS directors (
 		id INT AUTO_INCREMENT PRIMARY KEY,
 		name VARCHAR(100) NOT NULL,
+		password_hash VARCHAR(255) NOT NULL,
+		token VARCHAR(255) UNIQUE,
+		role ENUM('admin', 'user') NOT NULL DEFAULT 'user',
 		deleted_at TIMESTAMP NULL DEFAULT NULL
 	);
 `
