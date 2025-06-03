29bf7e000ae29a4f91c55c78f77af6f62e7fec9e
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-06-03 16:52:49 +0700

    refactor(challenge): add user_id for connection and remove answer from the json

--- andhika-pratama/uts-andhika-pratama/models/challenge_model.go
+++ andhika-pratama/uts-andhika-pratama/models/challenge_model.go
@@ -4,7 +4,8 @@ import "time"
 
 type Challenge struct {
 	ChallengeID int       `json:"challenge_id"`
+	UserID      int       `json:"user_id"`
 	Question    string    `json:"question"`
-	Answer      string    `json:"answer"`
+	Answer      string    `json:"-"`
 	CreatedAt   time.Time `json:"created_at"`
 }
