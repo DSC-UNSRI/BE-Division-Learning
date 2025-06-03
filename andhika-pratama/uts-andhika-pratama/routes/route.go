d38bce826a1abc665bd31c126307f15c912e2861
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-06-03 17:39:09 +0700

    feat(routes): set up auth routes

---
+++ andhika-pratama/uts-andhika-pratama/routes/route.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	// "net/http"
+)
+
+func setupRoutes() {
+	// http.HandleFunc("/register", controllers.Register)
+	// http.HandleFunc("/login", controllers.Login)
+	// http.HandleFunc("/forget", controllers.ForgetPassword)
+}
