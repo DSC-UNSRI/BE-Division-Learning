aeec6a56b1fb17f6c879492894ca4d79f164c088
artichys <kenzraki12@gmail.com>
2025-06-04 16:47:43 +0700

    feat(auth): implement speaker authentication middleware with context

--- kenz-raki-abdurrazak/TugasP5/routes/routes.go
+++ kenz-raki-abdurrazak/TugasP5/routes/routes.go
@@ -15,13 +15,13 @@ func InitRoutes() *mux.Router {
 	r.HandleFunc("/events/{id}", controllers.GetEventByID).Methods("GET")
 	r.HandleFunc("/speakers/{id}/events", controllers.GetEventsBySpeakerID).Methods("GET")
 
-	r.HandleFunc("/speakers", middleware.AuthMiddleware(controllers.CreateSpeaker)).Methods("POST")
-	r.HandleFunc("/speakers/{id}", middleware.AuthMiddleware(controllers.UpdateSpeaker)).Methods("PUT")
-	r.HandleFunc("/speakers/{id}", middleware.AuthMiddleware(controllers.DeleteSpeaker)).Methods("DELETE")
-	r.HandleFunc("/events", middleware.AuthMiddleware(controllers.CreateEvent)).Methods("POST")
-	r.HandleFunc("/events/{id}", middleware.AuthMiddleware(controllers.UpdateEvent)).Methods("PUT")
-	r.HandleFunc("/events/{id}", middleware.AuthMiddleware(controllers.DeleteEvent)).Methods("DELETE")
-	r.HandleFunc("/full-event", middleware.AuthMiddleware(controllers.CreateFullEvent)).Methods("POST")
+	r.HandleFunc("/speakers", middleware.AuthSpeakerMiddleware(controllers.CreateSpeaker)).Methods("POST")
+	r.HandleFunc("/speakers/{id}", middleware.AuthSpeakerMiddleware(controllers.UpdateSpeaker)).Methods("PUT")
+	r.HandleFunc("/speakers/{id}", middleware.AuthSpeakerMiddleware(controllers.DeleteSpeaker)).Methods("DELETE")
+	r.HandleFunc("/events", middleware.AuthSpeakerMiddleware(controllers.CreateEvent)).Methods("POST")
+	r.HandleFunc("/events/{id}", middleware.AuthSpeakerMiddleware(controllers.UpdateEvent)).Methods("PUT")
+	r.HandleFunc("/events/{id}", middleware.AuthSpeakerMiddleware(controllers.DeleteEvent)).Methods("DELETE")
+	r.HandleFunc("/full-event", middleware.AuthSpeakerMiddleware(controllers.CreateFullEvent)).Methods("POST")
 	
 	r.HandleFunc("/auth/login", controllers.LoginSpeaker).Methods("POST")
 
