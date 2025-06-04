f8f637a3e5845bc6a89c16aef6c3cc33f7a3445b
RenaiRS <dianramadhan71005@gmail.com>
2025-06-04 15:02:24 +0700

    chore(): rename routes

--- Dian-Kharisma-Ramadhan/Task_5/main.go
+++ Dian-Kharisma-Ramadhan/Task_5/main.go
@@ -13,14 +13,13 @@ import (
 func main() {
 	config.ENVLoad()
 	database.InitDB()
-	routes.RegisterNilaiMataUangRoutes()
-	routes.RegisterNasabahRoutes()
+	defer database.DB.Close()
+	routes.NasabahRoutes()
+	routes.TabunganRoutes()
 	log.Println("Server running on http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("Server error:", err)
 	}
-
 	fmt.Println("DB User:", os.Getenv("DB_USER"))
-
 }
