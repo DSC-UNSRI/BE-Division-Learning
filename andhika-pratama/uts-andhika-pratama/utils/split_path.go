8b88ccc7bf66d5e52c0d06dc66f7ef314171e88c
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-06-04 13:58:03 +0700

    fix(utils): split path only accepted url with lenghts of 3

--- andhika-pratama/uts-andhika-pratama/utils/split_path.go
+++ andhika-pratama/uts-andhika-pratama/utils/split_path.go
@@ -7,7 +7,7 @@ import (
 
 func SplitPath(path string) ([]string, error){
 	parts := strings.Split(path, "/")
-	if len(parts) != 3 {
+	if len(parts) > 4 {
 		return []string{} , errors.New("request is not valid")
 	}
 	return parts, nil
