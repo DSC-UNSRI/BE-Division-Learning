86e7ecc38cbbf9d2309b494abeaf836f6f4a27ff
mauinGit <maulanaadiatma3133@gmail.com>
2025-06-05 18:30:07 +0700

    feat (models): add table contents

--- Maulana-Adiatma/Resep_Makanan_with_Middleware/models/negara.go
+++ Maulana-Adiatma/Resep_Makanan_with_Middleware/models/negara.go
@@ -1,8 +1,11 @@
 package models
 
 type Negara struct {
-	ID        	int    	`json:"id"`
-	NamaNegara 	string 	`json:"negara_asal"`
-	KodeNegara	int 	`json:"kode_negara"`
-	AuthKey    	string 	`json:"auth_key"`
+    ID          int    `json:"id"`
+    NamaNegara  string `json:"negara_asal"`
+    KodeNegara  string `json:"kode_negara"`
+    Email       string `json:"email_users"`
+    Password    string `json:"-"`
+    Token       string `json:"token_users"`
+    Role        string `json:"role_users"`
 }
