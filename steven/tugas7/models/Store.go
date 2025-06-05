841b2068708cad34746eee0dd14ac88d048b7e3b
Steven <stevenbae9@gmail.com>
2025-06-05 15:37:35 +0700

    feat(models): add more struct for store

--- steven/tugas7/models/Store.go
+++ steven/tugas7/models/Store.go
@@ -1,10 +1,15 @@
 package models
 
+import "time"
+
 type Store struct {
 	ID      int    `json:"id"`
 	Name    string `json:"name"`
 	Owner   string `json:"owner"`
 	Password string `json:"password"`
+	Token     string     `json:"token"`
+	Role	  string     `json:"role"`
+	DeletedAt *time.Time `json:"deleted_at"`
 }
 
 var StoreQuery = `
@@ -13,4 +18,7 @@ var StoreQuery = `
     name VARCHAR(100),
     owner VARCHAR(100),
     password VARCHAR(100),
+	token VARCHAR(255) NOT NULL,
+	role ENUM('admin', 'user') NOT NULL DEFAULT 'user',
+	deleted_at TIMESTAMP NULL DEFAULT NULL
 	);`
