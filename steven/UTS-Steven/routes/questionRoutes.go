135218c802ea5fa8c4ce133adaafb4c2af093322
Steven <stevenbae9@gmail.com>
2025-06-23 23:36:32 +0700

    feat(routes): add more endpoint handler and auth

--- steven/UTS-Steven/routes/questionRoutes.go
+++ steven/UTS-Steven/routes/questionRoutes.go
@@ -1,15 +1,21 @@
 package routes
 
-import(
+import (
+	"net/http"
 	"uts-gdg/controllers"
 	"uts-gdg/middleware"
 	"uts-gdg/utils"
-	"net/http"
 )
 
 func QuestionRoutes(){
 	http.HandleFunc("/questions", questionHandler)
-	//http.HandleFunc("/questions/", questionHandlerWithID)
+	http.HandleFunc("/questions/", questionHandlerWithID)
+}
+
+func withAuth(handler http.HandlerFunc) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        utils.ApplyMiddlewares(handler, middleware.AuthMiddleware).ServeHTTP(w, r)
+    }
 }
 
 func withPremiumAuth(handler http.HandlerFunc) http.HandlerFunc {
@@ -24,6 +30,31 @@ func questionHandler(w http.ResponseWriter, r *http.Request) {
 		controllers.GetQuestions(w,r)
 	case http.MethodPost:
 		withPremiumAuth(controllers.CreateQuestions)(w,r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func questionHandlerWithID(w http.ResponseWriter, r *http.Request) {
+	parts, err := utils.SplitPath(r.URL.Path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	id := parts[2]
+
+	switch r.Method {
+	case http.MethodGet:
+		withAuth(func(w http.ResponseWriter, r *http.Request) {
+            controllers.GetQuestion(w, r, id)
+        })(w, r)
+	case http.MethodPatch:
+		withPremiumAuth(func(w http.ResponseWriter, r *http.Request) {
+            controllers.UpdateQuestion(w, r, id)
+        })(w, r)
+	case http.MethodDelete:
+		withPremiumAuth(func(w http.ResponseWriter, r *http.Request) {
+            controllers.DeleteQuestion(w, r, id)
+        })(w, r)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
