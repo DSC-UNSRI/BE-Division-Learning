2f46fd855a14dda3e55423f8f37356d0dd038306
Steven <stevenbae9@gmail.com>
2025-06-24 13:10:09 +0700

    feat(middleware): add tokenExpire handler in middleware

--- steven/UTS-Steven/middleware/middleware.go
+++ steven/UTS-Steven/middleware/middleware.go
@@ -2,7 +2,9 @@ package middleware
 
 import (
 	"context"
+	"database/sql"
 	"net/http"
+	"time"
 	"uts-gdg/database"
 	"uts-gdg/utils"
 )
@@ -27,12 +29,19 @@ func AuthMiddleware(next http.Handler) http.Handler {
 		var exists bool
 		var userID int
 		var role string
+		var tokenExpire sql.NullTime
+		
 		err := database.DB.QueryRow(
-			"SELECT EXISTS(SELECT 1 FROM users WHERE token = ? AND deleted_at IS NULL), id, role FROM users WHERE token = ? AND deleted_at IS NULL",
+			"SELECT EXISTS(SELECT 1 FROM users WHERE token = ? AND deleted_at IS NULL), id, role, token_expire FROM users WHERE token = ? AND deleted_at IS NULL",
 			token, token,
-		).Scan(&exists, &userID, &role)
+		).Scan(&exists, &userID, &role, &tokenExpire)
 		if err != nil || !exists {
 			http.Error(w, "Unauthorized - invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		if tokenExpire.Valid && time.Now().After(tokenExpire.Time) {
+			http.Error(w, "Unauthorized - token expired", http.StatusUnauthorized)
 			return
 		}
 
