5dbf30f0fcd0b16d1960eb2119dede29c6d45bae
barbarIQ <rihalsulaiman@gmail.com>
2025-06-25 15:41:32 +0700

    feat(middlewares): add OwnsQuestionMiddleware for question ownership verification

---
+++ Adhia-Rihal-Sulaiman/UTS-adhiarihalsulaiman/middlewares/question_middlewares.go
@@ -0,0 +1,44 @@
+package middlewares
+
+import (
+	"uts_adhia/database"
+	"uts_adhia/utils"
+
+	"net/http"
+	"database/sql"
+)
+
+func OwnsQuestionMiddleware(next http.Handler, questionID string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctxUserID := r.Context().Value(utils.UserIDKey)
+		userID, ok := ctxUserID.(string)
+		if !ok || userID == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		if questionID == "" {
+			http.Error(w, "question_id is null", http.StatusBadRequest)
+			return
+		}
+
+		var questionOwnerID string
+		err := database.DB.QueryRow("SELECT user_id FROM questions WHERE question_id = ? AND deleted_at IS NULL", questionID).Scan(&questionOwnerID)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				http.Error(w, "Question not found", http.StatusNotFound)
+			} else {
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			}
+			return
+		}
+
+
+		if questionOwnerID != userID {
+			http.Error(w, "Forbidden - the question isn't yours", http.StatusForbidden)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
