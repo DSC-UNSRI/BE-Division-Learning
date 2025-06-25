135d076a9f638df10773b830f20fc56da6e7e12d
barbarIQ <rihalsulaiman@gmail.com>
2025-06-25 15:41:25 +0700

    feat(middlewares): implement various authentication middlewares for access control

---
+++ Adhia-Rihal-Sulaiman/UTS-adhiarihalsulaiman/middlewares/permission_middlewares.go
@@ -0,0 +1,76 @@
+package middlewares
+
+import (
+	"uts_adhia/utils"
+
+	"net/http"
+)
+
+func WithAuth(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		utils.ApplyMiddlewares(handler, AuthMiddleware).ServeHTTP(w, r)
+	}
+}
+
+func WithPremiumAuth(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		utils.ApplyMiddlewares(handler, AuthMiddleware, TypeMiddleware).ServeHTTP(w, r)
+	}
+}
+
+func WithAdminAuth(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		utils.ApplyMiddlewares(handler, AuthMiddleware, RoleMiddleware).ServeHTTP(w, r)
+	}
+}
+
+func WithAdminOrPremiumAuth(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+	utils.ApplyMiddlewares(handler, AuthMiddleware, HackerMiddleware).ServeHTTP(w, r)
+	}
+}
+
+func WithOwnsQuestionAuth(handler func(http.ResponseWriter, *http.Request, string), id string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		wrappedControllerHandler := http.HandlerFunc(func(innerW http.ResponseWriter, innerR *http.Request) {
+			handler(innerW, innerR, id)
+		})
+
+		middlewareChain := OwnsQuestionMiddleware(
+			wrappedControllerHandler,
+			id,
+		)
+
+		utils.ApplyMiddlewares(middlewareChain, AuthMiddleware).ServeHTTP(w, r)
+	}
+}
+
+func WithOwnsAnswerAuth(handler func(http.ResponseWriter, *http.Request, string), id string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		wrappedControllerHandler := http.HandlerFunc(func(innerW http.ResponseWriter, innerR *http.Request) {
+			handler(innerW, innerR, id)
+		})
+
+		middlewareChain := OwnsAnswerMiddleware(
+			wrappedControllerHandler,
+			id,
+		)
+
+		utils.ApplyMiddlewares(middlewareChain, AuthMiddleware).ServeHTTP(w, r)
+	}
+}
+
+func WithOwnsHighlightAuth(handler func(http.ResponseWriter, *http.Request, string), id string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		wrappedControllerHandler := http.HandlerFunc(func(innerW http.ResponseWriter, innerR *http.Request) {
+			handler(innerW, innerR, id)
+		})
+
+		middlewareChain := OwnsHighlightMiddleware(
+			wrappedControllerHandler,
+			id,
+		)
+
+		utils.ApplyMiddlewares(middlewareChain, AuthMiddleware).ServeHTTP(w, r)
+	}
+}
