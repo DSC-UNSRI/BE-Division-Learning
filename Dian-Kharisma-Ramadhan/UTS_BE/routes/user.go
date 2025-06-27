9a1ab07521058beafe982315af1012e60ce91a09
RenaiRS <dianramadhan71005@gmail.com>
2025-06-27 18:23:37 +0700

    refactor(routes): updating routes user.go

--- Dian-Kharisma-Ramadhan/UTS_BE/routes/user.go
+++ Dian-Kharisma-Ramadhan/UTS_BE/routes/user.go
@@ -3,12 +3,16 @@ package routes
 import (
 	"net/http"
 	"UTS_BE/controllers"
+	"UTS_BE/middleware"
 )
 
 func AuthRoutes() {
 	http.HandleFunc("/auth/register", controllers.RegisterUser)
 	http.HandleFunc("/auth/login", controllers.LoginUser)
 	http.HandleFunc("/auth/reset-password", controllers.ResetPassword)
+	http.HandleFunc("/auth/logout", middleware.WithAuth(controllers.LogoutUser))
+	http.HandleFunc("/auth/profile", middleware.WithAuth(controllers.GetProfile))
+
 }
 
 
