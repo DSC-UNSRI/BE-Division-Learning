f866de684141c17d59ff27d5064754d0fd0f5628
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-08-27 21:58:50 +0700

    feat(models): add user and event models

---
+++ andhika-pratama/pert12/backend1/models/event_model.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Event struct {
+	gorm.Model
+	Location string `json:"location" gorm:"not null"`
+	Start time.Time `json:"start" gorm:"not null"`
+	Cover string `json:"cover"`
+}
---
+++ andhika-pratama/pert12/backend1/models/user_model.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name           string `json:"name" gorm:"not null"`
+	Password       string `json:"passsword" gorm:"not null"`
+	ProfilePicture string `json:"profile_picture"`
+	Email          string `json:"email" gorm:"unique;not null"`
+	Role           string `json:"role" gorm:"type:varchar(10);default:'user';not null"`
+}
+
+type UserLogin struct {
+	gorm.Model
+	Role           string `json:"role" gorm:"type:varchar(10);default:'user';not null"`
+}
