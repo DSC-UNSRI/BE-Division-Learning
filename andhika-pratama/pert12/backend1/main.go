2e3a127637d341d2b90e457faec769e73c9bb8ae
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-08-27 20:55:46 +0700

    feat: setup database connection

--- andhika-pratama/pert12/backend1/main.go
+++ andhika-pratama/pert12/backend1/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"pert12/config"
+	"pert12/database"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	config.ENVLoad()
-	
+	database.DBLoad()
+
 	app := fiber.New()
 
 	app.Listen(":8080")
