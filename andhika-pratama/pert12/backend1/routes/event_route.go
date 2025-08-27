512c3792410af3aac49966553575399d7ca796b8
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-08-27 22:43:56 +0700

    feat(routes): set up routing for auth, user, and event

---
+++ andhika-pratama/pert12/backend1/routes/event_route.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	// "pert12/controllers"
+	// "pert12/middlewares"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func EventRoutes(api fiber.Router) {
+	// api.Get("/event", middlewares.AdminAuth, controllers.GetEvents)
+	// api.Post("/event", middlewares.AdminAuth, controllers.PostEvent)
+	// api.Patch("/event/:id", middlewares.AdminAuth, controllers.UpdateEvent)
+	// api.Delete("/event/:id", middlewares.AdminAuth, controllers.DeleteEvent)
+}
---
+++ andhika-pratama/pert12/backend1/routes/user_route.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	// "pert12/controllers"
+	// "pert12/middlewares"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func UserRoutes(api fiber.Router) {
+	// api.Get("/me", middlewares.Auth, controllers.GetMe)
+	// api.Patch("/profile/:id", middlewares.Auth, controllers.UpdateProfile)
+}
