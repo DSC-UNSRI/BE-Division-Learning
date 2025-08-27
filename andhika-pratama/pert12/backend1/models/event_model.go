1ed88051a3a989be49efce94545b365837db8784
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-08-28 01:01:19 +0700

    fix(models): correct the default assets naming

--- andhika-pratama/pert12/backend1/models/event_model.go
+++ andhika-pratama/pert12/backend1/models/event_model.go
@@ -10,5 +10,5 @@ type Event struct {
 	gorm.Model
 	Location string `json:"location" gorm:"not null"`
 	Start time.Time `json:"start" gorm:"not null"`
-	Cover string `json:"cover" gorm:"default:'/assets/covers/hinamizawa.jpg'"`
+	Cover string `json:"cover" gorm:"default:'http://127.0.0.1:3000/assets/covers/hinamizawa.jpg'"`
 }
