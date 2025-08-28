aa5935e3a955bd34290910b72be2d4ddf60efe3b
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-08-28 08:16:31 +0700

    refactor(main): change backend server port

--- andhika-pratama/pert12/backend1/main.go
+++ andhika-pratama/pert12/backend1/main.go
@@ -16,11 +16,11 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000, http://localhost:5173/",
+		AllowOrigins:     http://localhost:5173/",
 		AllowCredentials: true,
 	}))
 	app.Static("/assets", "./assets")
 
 	routes.SetupRoutes(app)
-	app.Listen(":8080")
+	app.Listen(":3000")
 }
