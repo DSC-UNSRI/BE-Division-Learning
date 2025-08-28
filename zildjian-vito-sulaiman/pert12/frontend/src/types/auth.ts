9b0d0320f8ebf3f8b09208da6a6f511433bdf9b6
zildjianvitoo <zildjianvito07@gmail.com>
2025-08-28 16:12:34 +0700

    feat(pert12): add new backend and frontend project

---
+++ zildjian-vito-sulaiman/pert12/frontend/src/types/auth.ts
@@ -0,0 +1,18 @@
+import type { User } from "./user";
+
+export type LoginResponse = {
+  message: string;
+  user: {
+    id: number;
+    role: "user" | "admin";
+  };
+};
+
+export type SignupResponse = {
+  message: string;
+  user: User;
+};
+
+export type LogoutResponse = {
+  message: string;
+};
