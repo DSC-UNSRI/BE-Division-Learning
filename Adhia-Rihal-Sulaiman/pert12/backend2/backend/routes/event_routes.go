7b08c16dcaf5c3fdf09d82be1c7b62e4b9fb2ebe
barbarIQ <rihalsulaiman@gmail.com>
2025-08-28 20:35:22 +0700

    feat(routes): update event routes to use correct controller methods for patch and delete

--- Adhia-Rihal-Sulaiman/pert12/backend2/backend/routes/event_routes.go
+++ Adhia-Rihal-Sulaiman/pert12/backend2/backend/routes/event_routes.go
@@ -10,6 +10,6 @@ import (
 func eventRoutes(api fiber.Router) {
 	api.Get("/event", controllers.GetEvent)
 	api.Post("/event", controllers.PostEvent, middlewares.AdminMiddleware(), middlewares.AuthMiddleware())
-	api.Patch("/event/:id", controllers.PostEvent, middlewares.AdminMiddleware(), middlewares.AuthMiddleware())
-	api.Delete("/event/:id", controllers.PostEvent, middlewares.AdminMiddleware(), middlewares.AuthMiddleware())
+	api.Patch("/event/:id", controllers.UpdateEvent, middlewares.AdminMiddleware(), middlewares.AuthMiddleware())
+	api.Delete("/event/:id", controllers.DeleteEvent, middlewares.AdminMiddleware(), middlewares.AuthMiddleware())
 }
