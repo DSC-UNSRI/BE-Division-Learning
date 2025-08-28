e0afcc88ebc0115c44b026cb1186d92a97520bb0
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-08-28 10:15:02 +0700

    refactor(models): change how the variables' being handled

--- andhika-pratama/pert12/backend1/models/event_model.go
+++ andhika-pratama/pert12/backend1/models/event_model.go
@@ -7,8 +7,11 @@ import (
 )
 
 type Event struct {
-	gorm.Model
-	Location string `json:"location" gorm:"not null"`
-	Start time.Time `json:"start" gorm:"not null"`
-	Cover string `json:"cover" gorm:"default:'http://127.0.0.1:3000/assets/covers/hinamizawa.jpg'"`
+	ID        int            `json:"id" gorm:"primarykey"`
+	Location  string         `json:"location" gorm:"not null"`
+	Start     time.Time      `json:"start" gorm:"not null"`
+	Cover     string         `json:"cover" gorm:"default:'http://127.0.0.1:3000/assets/covers/hinamizawa.jpg'"`
+	CreatedAt time.Time      `json:"-"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
 }
--- andhika-pratama/pert12/backend1/models/user_model.go
+++ andhika-pratama/pert12/backend1/models/user_model.go
@@ -1,23 +1,31 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
-	Name           string `json:"name" gorm:"not null"`
-	Password       string `json:"passsword" gorm:"not null"`
-	ProfilePicture string `json:"profile_picture" gorm:"default:'http://127.0.0.1:3000/assets/profile_pictures/auby.jpeg'"`
-	Email          string `json:"email" gorm:"unique;not null"`
-	Role           string `json:"role" gorm:"type:varchar(10);default:'user';not null"`
+	ID             int            `json:"id" gorm:"primarykey"`
+	Name           string         `json:"name" gorm:"not null"`
+	Password       string         `json:"passsword" gorm:"not null"`
+	ProfilePicture string         `json:"profile_picture" gorm:"default:'http://127.0.0.1:3000/assets/profile_pictures/auby.jpeg'"`
+	Email          string         `json:"email" gorm:"unique;not null"`
+	Role           string         `json:"role" gorm:"type:varchar(10);default:'user';not null"`
+	CreatedAt      time.Time      `json:"-"`
+	UpdatedAt      time.Time      `json:"-"`
+	DeletedAt      gorm.DeletedAt `json:"-" gorm:"index"`
 }
 
 type UserLogin struct {
-	gorm.Model
-	Role           string `json:"role" gorm:"type:varchar(10);default:'user';not null"`
+	ID        int            `json:"id" gorm:"primarykey"`
+	Role      string         `json:"role" gorm:"type:varchar(10);default:'user';not null"`
+	CreatedAt time.Time      `json:"-"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
 }
 
 func (User) TableName() string {
-    return "user"
+	return "user"
 }
