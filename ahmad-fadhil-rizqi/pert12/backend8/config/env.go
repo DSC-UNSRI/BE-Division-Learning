6e0d6ccbc1ffd689ba19fac4433c839054353765
fadhilrizqi1 <fadhilrizqi2006@gmail.com>
2025-08-29 17:21:26 +0700

    refactor(config): use godotenv with ENVLoad

--- ahmad-fadhil-rizqi/pert12/backend8/config/env.go
+++ ahmad-fadhil-rizqi/pert12/backend8/config/env.go
@@ -1,35 +1,9 @@
 package config
 
-import (
-	"log"
-	"os"
+import "github.com/joho/godotenv"
 
-	"github.com/joho/godotenv"
-)
-
-type Config struct {
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
-	BaseURL    string
-	JWTSecret  string
-}
-
-func Load() Config {
+func ENVLoad() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+		panic("Error loading .env file")
 	}
-
-	config := Config{
-		DBHost:     os.Getenv("DB_HOST"),
-		DBPort:     os.Getenv("DB_PORT"),
-		DBUser:     os.Getenv("DB_USER"),
-		DBPassword: os.Getenv("DB_PASSWORD"),
-		DBName:     os.Getenv("DB_NAME"),
-		BaseURL:    os.Getenv("BASE_URL"),
-		JWTSecret:  os.Getenv("JWT_SECRET"),
-	}
-	return config
 }
